163f1559913bab1a75e82654236fcf340cd9f456
david <miharisoadavidfils@gmail.com>
2025-05-27 13:15:58 +0300

    feat(loops): add for, while, do-while, and nested loop examples

---
+++ src/loops/WhileLoopDemo.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // Loop while i is less than or equal to 5
+        while (i <= 5) {
+            System.out.println("i = " + i);
+            i++;
+        }
+    }
+}
