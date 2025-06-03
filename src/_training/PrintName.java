c85a48b7279e0836e466840584e357320c130091
david <miharisoadavidfils@gmail.com>
2025-06-03 12:24:22 +0300

    [ADD] feature(training): SimpleCalculator: +, -, *, /

--- src/_training/PrintName.java
+++ src/_training/PrintName.java
@@ -1,4 +1,12 @@
 package _training;
 
 public class PrintName {
+    public static void main(String[] args) {
+        String name = "myName";
+        System.out.println(String.format("""
+                Hello world !!!, 
+                My name is %s!
+                And I love Java
+                """, name));
+    }
 }
---
+++ src/_training/SimpleCalculator.java
@@ -0,0 +1,55 @@
+package _training;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static String addition(int a, int b) {
+        return a + " + " + b + " = " + (a + b);
+    }
+    public static String soustraction(int a, int b) {
+        return a + " - " + b + " = " + (a - b);
+    }
+    public static String multiply(int a, int b) {
+        return a + " * " + b + " = " + (a * b);
+    }
+    public static String divide(int a, int b) {
+        if (b == 0) {
+            return "Error, we can't divide by zero!";
+        }else {
+            return a + " / " + b + " = " + (a / b);
+        }
+    }
+
+    public static void main(String[] args) {
+        String result = "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.print("Enter operation (+, -, *, /): ");
+        String operation = scanner.next();
+
+        switch (operation) {
+            case "+":
+                result = addition(firstNumber, secondNumber);
+                break;
+            case "-":
+                result = soustraction(firstNumber, secondNumber);
+                break;
+            case "*":
+                result = multiply(firstNumber, secondNumber);
+                break;
+            case "/":
+                result = divide(firstNumber, secondNumber);
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+
+        System.out.println(result);
+    }
+}
